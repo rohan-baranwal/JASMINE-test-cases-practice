fb6371f6a160e80de3f10bd2bb3f330dc8584f52
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-10-21 09:34:47 +0530

    displaying result on page

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 const calculate = (event) => {
   const inputValue = event.target.value;
-  const regExp = /\+\-\*\//;
+  const regExp = /\+|\-|\*|\//;
 
   const numberA = +inputValue.split(regExp)[0];
   const numberB = +inputValue.split(regExp)[1];
   const operator = inputValue.match(regExp)[0];
 
   const calculator = new Calculator();
+  calculator.add(numberA);
 
+  let result;
+  switch (operator) {
+    case "+":
+      result = calculator.add(numberB);
+      break;
+    case "-":
+      result = calculator.subtract(numberB);
+      break;
+    case "*":
+      result = calculator.multiply(numberB);
+      break;
+    case "/":
+      result = calculator.divide(numberB);
+      break;
+
+    default:
+      break;
+  }
+  updateResult(result);
+};
+
+const updateResult = (result) => {
+  let element = document.getElementById("result");
+  if (element) {
+    element.innerText = result;
+  }
 };
 document.getElementById("inputValue").addEventListener("change", calculate);
