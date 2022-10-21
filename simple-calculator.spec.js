6d8daf31043fd70a70720fdf35112ebce0e68f89
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-10-21 11:26:20 +0530

    added jasmine config files

--- simple-calculator.spec.js
+++ simple-calculator.spec.js
@@ -1,2 +1,12 @@
 // test suite
 // Suite --> group of tests
+
+describe("simple-calculator", () => {
+  beforeEach(() => {});
+  it("should add number to the total", () => {
+    
+  });
+  it("should expect true to be true", () => {
+    expect(true).toBeTrue();
+  });
+});
