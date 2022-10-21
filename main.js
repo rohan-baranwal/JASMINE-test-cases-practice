a6b1f961a0e8360f0d710b0d10820fc32d3bc570
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-10-21 09:18:26 +0530

    extraction of numbers and operators using reg exp

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const calculate = (event) => {
   const inputValue = event.target.value;
   const regExp = /\+\-\*\//;
-  let numberA, numberB, operator;
+
+  const numberA = +inputValue.split(regExp)[0];
+  const numberB = +inputValue.split(regExp)[1];
+  const operator = inputValue.match(regExp)[0];
+
+  const calculator = new Calculator();
 
 };
 document.getElementById("inputValue").addEventListener("change", calculate);
