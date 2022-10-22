97fee59ff43d98a358a486762ce962b0c26c385e
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-10-22 11:57:14 +0530

    matchers

--- simple-calculator.spec.js
+++ simple-calculator.spec.js
@@ -10,23 +10,27 @@ describe("simple-calculator", () => {
     calculator.subtract(1);
     expect(calculator.total).toEqual(4);
   });
+
   it("should subtract number from the total", () => {
     const calculator = new Calculator();
     calculator.subtract(5);
     expect(calculator.total).toBe(-5);
   });
+
   it("should multiply number to the total", () => {
     const calculator = new Calculator();
     calculator.total = 5;
     calculator.multiply(5);
     expect(calculator.total).toBe(25);
   });
+
   it("should divide the total by number", () => {
     const calculator = new Calculator();
     calculator.total = 5;
     calculator.divide(5);
     expect(calculator.total).toBe(1);
   });
+
   it("should expect true to be true", () => {
     expect(true).toBeTrue();
   });
