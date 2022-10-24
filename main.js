dead13ad81c0435af5be069ff71f4d1deebf092b
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-10-24 07:44:52 +0530

    callFake() and callThrough()

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const calculate = (event) => {
-  const inputValue = event.target.value;
+function calculate(val) {
+  const inputValue = val;
   const regExp = /\+|\-|\*|\//;
 
   const numberA = +inputValue.split(regExp)[0];
   const numberB = +inputValue.split(regExp)[1];
   const operation = inputValue.match(regExp);
-  const operator = operation[0];
-  if (operator === null || isNaN(numberA) || isNaN(numberB)) {
+  if (
+    operation === null ||
+    operation === undefined ||
+    isNaN(numberA) ||
+    isNaN(numberB)
+  ) {
     updateResult("Expression not recognized");
-    // throw new Error('provide a valid operator')
+    return;
   }
+  const operator = operation[0];
 
   const calculator = new Calculator();
   calculator.add(numberA);
@@ -34,14 +39,16 @@ const calculate = (event) => {
       break;
   }
   updateResult(result);
-};
+}
 
-const updateResult = (result) => {
+function updateResult(result) {
   let element = document.getElementById("result");
   if (element) {
     element.innerText = result;
   }
-};
+}
 
 document.getElementById("inputValue") &&
-  document.getElementById("inputValue").addEventListener("change", calculate);
+  document.getElementById("inputValue").addEventListener("change", (event) => {
+    calculate(event.target.value);
+  });
